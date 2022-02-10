6c90ad6650caf712761954f773d12f93ee5053b9
ferahar <ferahar@gmail.com>
2022-02-10 12:18:31 +0500

    add type remote

---
+++ shared/types/remote_types/remote_template.d.ts
@@ -0,0 +1,16 @@
+/// <reference types="react" />
+
+declare module 'remote_template/Kfa' {
+  const Kfa: React.FC<T>;
+  export default Kfa;
+}
+
+declare module 'remote_template/KfaToolbar' {
+  const KfaToolbar: React.FC<T>;
+  export default KfaToolbar;
+}
+
+declare module 'remote_template/routes' {
+  const remoteRoutes: unknown[];
+  export default remoteRoutes;
+}
